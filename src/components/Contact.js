a557d2c253a904a3e00c95c40bb2d06d217ad8e8
windi <windi.azmy@gmail.com>
2022-06-26 11:38:50 +0700

    complete task

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,18 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <section className="contact">
+            <div className="contact__image">
+                <img className="img" alt="gambar" src={data.photo}/>
+            </div>
+            <div className="contact__text">
+              <p className="contact__text--name">{data.name}</p>
+              <p className="contact__text--number">{data.phone}</p>
+              <p className="contact__text--email">{data.email}</p>
+            </div>
+        </section>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+       <header className="header">
+          <h1 className="header__title">Call a Friend</h1>
+          <p className="header__subtitle">your friendly contact app</p>
+       </header>
     )
 }
 
